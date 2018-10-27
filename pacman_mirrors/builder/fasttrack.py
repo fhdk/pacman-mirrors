55f96da3b850a394cdbcebe52111caa83f3d0a03
Frede Hundewadt <fh@manjaro.org>
2018-10-27 09:54:40 +0200

    added hints named arguments

--- pacman_mirrors/builder/fasttrack.py
+++ pacman_mirrors/builder/fasttrack.py
@@ -30,7 +30,7 @@ from pacman_mirrors.functions import util
 from pacman_mirrors.functions.sortMirrorFn import sort_mirrors
 
 
-def build_mirror_list(self, limit):
+def build_mirror_list(self, limit: int) -> None:
     """
     Fast-track the mirrorlist by filtering only up-to-date mirrors
     The function takes into account the branch selected by the user
@@ -42,42 +42,44 @@ def build_mirror_list(self, limit):
     """
     remove known bad mirrors (status.json last_sync = -1)
     """
-    worklist = filterFn.filter_bad_mirrors(self.mirrors.mirror_pool)
+    worklist = filterFn.filter_bad_mirrors(
+        mirror_pool=self.mirrors.mirror_pool)
     """
     filter protocols if user has a selection
     """
     if self.config["protocols"]:
         worklist = filterFn.filter_mirror_protocols(
-            worklist, self.config["protocols"])
+            mirror_pool=worklist, protocols=self.config["protocols"])
 
     """
     Only pick mirrors which are up-to-date for the system branch
     by removing not up-to-date mirrors from the list
     """
-    up_to_date_mirrors = filterFn.filter_user_branch(worklist, self.config)
+    up_to_date_mirrors = filterFn.filter_user_branch(
+        mirror_pool=worklist, config=self.config)
     """
     Shuffle the list
     """
     shuffle(up_to_date_mirrors)
-    util.msg(message=f"{txt.QUERY_MIRRORS} - {txt.TAKES_TIME}",
-             urgency=txt.INF_CLR,
-             tty=self.tty)
+    util.msg(
+        message=f"{txt.QUERY_MIRRORS} - {txt.TAKES_TIME}", urgency=txt.INF_CLR, tty=self.tty)
 
     # probe the mirrors
-    worklist = testMirrorFn.test_mirrors(self, up_to_date_mirrors, limit)
+    worklist = testMirrorFn.test_mirrors(
+        self=self, worklist=up_to_date_mirrors, limit=limit)
     # sort the result
-    worklist = sort_mirrors(worklist, field="resp_time", reverse=False)
+    worklist = sort_mirrors(
+        worklist=worklist, field="resp_time", reverse=False)
 
     """
     Write mirrorlist
     """
     try:
         _ = worklist[0]
-        outputFn.file_mirror_list(self, worklist)
+        outputFn.file_mirror_list(
+            self=self, selected_servers=worklist)
     except IndexError:
-        util.msg(message=f"{txt.NO_SELECTION}",
-                 urgency=txt.WRN_CLR,
-                 tty=self.tty)
-        util.msg(message=f"{txt.NO_CHANGE}",
-                 urgency=txt.INF_CLR,
-                 tty=self.tty)
+        util.msg(
+            message=f"{txt.NO_SELECTION}", urgency=txt.WRN_CLR, tty=self.tty)
+        util.msg(
+            message=f"{txt.NO_CHANGE}", urgency=txt.INF_CLR, tty=self.tty)
