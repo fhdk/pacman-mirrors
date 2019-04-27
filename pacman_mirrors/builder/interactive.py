07a13c9f95d485b3624ed8f7166c98dad3c17724
Frede Hundewadt <fh@manjaro.org>
2019-04-27 18:59:49 +0200

    refactor builder to add code reuse

--- pacman_mirrors/builder/interactive.py
+++ pacman_mirrors/builder/interactive.py
@@ -22,16 +22,14 @@
 from operator import itemgetter
 from random import shuffle
 
-from pacman_mirrors.functions.filter_mirror_status_functions import \
-    filter_bad_mirrors, filter_error_mirrors, filter_poor_mirrors
-
+from pacman_mirrors.builder.pool_builder import build_pool
 from pacman_mirrors.constants import txt
 
 from pacman_mirrors.functions.convertFn import \
     translate_interactive_to_pool, translate_pool_to_interactive
 
 from pacman_mirrors.functions.filter_mirror_pool_functions import \
-    filter_mirror_country, filter_mirror_protocols, filter_user_branch
+    filter_user_branch
 
 from pacman_mirrors.functions.outputFn import \
     write_custom_mirrors_json, write_pacman_mirror_list
@@ -43,15 +41,16 @@ from pacman_mirrors.functions import util
 
 def build_mirror_list(self) -> None:
     """
-    Prompt the user to select the mirrors with a gui.
+    Prompt the user to select the mirrors with an interface.
     Outputs a "custom" mirror file
     Outputs a pacman mirrorlist,
     """
 
-    worklist = build_working_pool(self)
+    worklist = build_pool(self)
 
     # rank or shuffle the mirrorlist before showing the ui
     if not self.default:
+        # if not default run method before selection
         if self.config["method"] == "rank":
             worklist = test_mirror_pool(self=self, worklist=worklist)
             worklist = sort_mirror_pool(worklist=worklist, field="resp_time", reverse=False)
@@ -68,22 +67,15 @@ def build_mirror_list(self) -> None:
         "last_sync": "HHh MMm",
         "url": "http://server/repo/"
     }
-    Therefore we have to create a list in the old format,
-    thus avoiding rewrite of the ui and related functions.
-    We subseqently need to translate the result into:
-    a. a mirrorfile in the new json format,
-    b. a mirrorlist in pacman format.
-    As of version 4.8.x the last sync field contents is a string
-    with the hours and minutes more human readable eg. 03h 33m
     """
     interactive_list = translate_pool_to_interactive(mirror_pool=worklist, tty=self.tty)
 
-    # import the right ui
+    # import the correct ui
     if self.no_display:
         # in console mode
         from pacman_mirrors.dialogs import consoleui as ui
     else:
-        # gobject introspection is present and accounted for
+        # gtk mode
         from pacman_mirrors.dialogs import graphicalui as ui
 
     interactive = ui.run(server_list=interactive_list, random=self.config["method"] == "random",
@@ -102,6 +94,7 @@ def build_mirror_list(self) -> None:
         """
         try:
             _ = mirror_list[0]
+            # using the default runs method after selection
             if self.default:
                 if self.config["method"] == "rank":
                     mirror_list = test_mirror_pool(self=self, worklist=mirror_list)
@@ -126,15 +119,6 @@ def build_mirror_list(self) -> None:
             write_custom_mirrors_json(self=self, selected_mirrors=custom_pool)
 
             """
-            Unless the user has provided the --no-status argument we only 
-            write mirrors which are up-to-date for users selected branch
-            """
-            if self.no_status:
-                pass
-            else:
-                mirror_list = filter_user_branch(mirror_pool=mirror_list, config=self.config)
-
-            """
             Writing mirrorlist
             If the mirror list is empty because 
             no up-to-date mirrors exist for users branch
@@ -151,40 +135,3 @@ def build_mirror_list(self) -> None:
         except IndexError:
             util.msg(message=f"{txt.NO_SELECTION}", urgency=txt.WRN_CLR, tty=self.tty)
             util.msg(message=f"{txt.NO_CHANGE}", urgency=txt.INF_CLR, tty=self.tty)
-
-
-def build_working_pool(self) -> list:
-    """
-    Apply country filter
-    """
-    pool_result = filter_mirror_country(mirror_pool=self.mirrors.mirror_pool, country_pool=self.selected_countries)
-
-    """
-    Apply bad mirrors filter
-    """
-    pool_result = filter_bad_mirrors(mirror_pool=pool_result)
-
-    """
-    Apply error mirrors filter
-    """
-    pool_result = filter_error_mirrors(mirror_pool=pool_result)
-
-    """
-    Apply protocol filter
-    If config.protols has content, that is a user decision and as such
-    it has nothing to do with the reasoning regarding mirrors
-    which might or might not be up-to-date
-    """
-    try:
-        _ = self.config["protocols"][0]
-        pool_result = filter_mirror_protocols(mirror_pool=pool_result, protocols=self.config["protocols"])
-    except IndexError:
-        pass
-
-    """
-    Apply interval filter
-    """
-    if self.no_status and self.interval:
-        pool_result = filter_poor_mirrors(mirror_pool=pool_result, interval=self.interval)
-
-    return pool_result
