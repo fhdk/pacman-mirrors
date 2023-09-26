b738a1fa88ec92c844fd39d8530be94a06dc01f3
Frede Hundewadt <noreply@wonky.codeberg.org>
2023-09-26 14:54:19 +0200

    WIP fix mirror list output

--- pacman_mirrors/functions/fileFn.py
+++ pacman_mirrors/functions/fileFn.py
@@ -95,7 +95,7 @@ def write_mirror_list(config: dict, servers: list, tty: bool = False, custom: bo
             write_mirrorlist_header(outfile, custom=custom)
             cols, lines = pacman_mirrors.functions.util.terminal_size()
             for server in servers:
-
+                print(server)
                 server["url"] = f'{util.sanitize_url(server["url"])}{config["branch"]}{config["repo_arch"]}'
                 if server["speed"] == 99.99:
                     # do not write bad servers to mirrorlist
